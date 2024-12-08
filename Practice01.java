ddfcea0cb6bd78ebe4bbdbc2af89b62ded937d59
sushanth123379 <sushanth48991@gmail.com>
2024-12-09 05:00:27 +0530

    Updated Practice01.java with new logic

--- Practice01.java
+++ Practice01.java
@@ -1,1 +1,25 @@
 
+public class Practice01{
+    public String compressString(String word) {
+        int i = 0, j = 0;
+        int n = word.length();
+        StringBuilder s = new StringBuilder();
+
+        while (i < n) {
+            int c = 0;
+            while (j < n && word.charAt(i) == word.charAt(j) && c < 9) {
+                c++;
+                j++;
+            }
+            s.append(c).append(word.charAt(i));
+            i = j;
+        }
+
+        return s.toString();
+    }
+
+    public static  void main(String[] args) {
+        String word="aaabbc";
+         System.out.println(new Practice01().compressString(word));
+    }
+}
